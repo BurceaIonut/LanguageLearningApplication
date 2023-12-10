6ba49211ff1d28a911bb2678d1af3b1a0b5c5f88
Burcea Ionut <ionut300burcea@gmail.com>
2023-12-11 00:11:00 +0200

    finally restored

---
+++ WpfApp2/View/Home.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+//Data Source = DESKTOP - MLLONH5; Initial Catalog = LanguageLearningApp; Integrated Security = True
+namespace WpfApp2.View
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        public Home()
+        {
+            InitializeComponent();
+
+            var converter = new BrushConverter();
+            ObservableCollection<Member> members = new ObservableCollection<Member>();
+
+            members.Add(new Member { Number = "1", Character = "J", BgColor = (Brush)converter.ConvertFromString("#1098AD"), Name = "John Doe", Position = "Coach", Email = "john.doe@gmail.com", Phone = "415-954-1475" });
+
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginView lg = new LoginView();
+            lg.Show();
+            Close();
+        }
+
+        private void BtnShowCourses_Click(object sender, RoutedEventArgs e)
+        {
+            FrameMain.Content=new PageUserInfo();
+        }
+
+        private void BtnUser_Click(object sender, RoutedEventArgs e)
+        {
+            FrameMain.Content = new PageCourses();
+        }
+    }
+    public class Member
+    {
+        public string Character { get; set; }
+        public Brush BgColor { get; set; }
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
